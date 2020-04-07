67237ab5b6b3ea1b0ec333dff80724e8c622b52b
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-04-07 19:28:43 +0100

    iterating fn added data in json file

--- index.js
+++ index.js
@@ -2,28 +2,131 @@
 
 // Above code lets the applications know that it needs to be excuted with node
 //  chmod +x ./index.js run this in terminal inorder to run app in cli
-const axios = require(axios);
+const axios = require('axios');
 const readline = require('readline').createInterface({
     input: process.stdin,
-    output: process.stdout
-})
+    output: process.stdout,
+    prompt: 'enter command >'
 
+});
 // The above code means it takes in input from the terminal and prints to the terminal
 
-readline.question('What would you like to log today?', async (item)=> {
-    const { data } = axios.get("http://localhost:3001/users");
-    const it = data[Symbol.iterator]();
+readline.prompt();
 
-    let position = it.next();
-    while(!position.done) {
-        const food = position.data.value;
-        if (food === item) {
-            console.log(`${item} has ${position.value.calories} calories`)
+readline.on('line', async (line) => {
+
+
+    switch (line.trim()) {
+        case 'list vegan foods':
+            {
+
+                axios.get(`http://localhost:3001/food`).then(({ data }) => {
+                    let idx = 0;
+
+                    const veganOnly = data.filter(food => {
+                        return food.dietary_preferences.includes('vegan')
+                    })
+                    const veganIterable = {
+                        [Symbol.iterator]() {
+                            return {
+                                [Symbol.iterator](){ return this;},
+                                next() {
+                                    const current = veganOnly[idx];
+                                    idx++
+
+                                    if(current) {
+                                        return { value: current, done: false}
+                                    } else {
+                                        return { value: current, done: true}
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                   
+                    for (let val of veganIterable) {
+                
+                        console.log(val.name);
+                    }
+                
+                    readline.prompt()
+            });
+
+            }
+            break;
+        case 'log': 
+            const { data } = await axios.get("http://localhost:3001/food");
+            const it = data[Symbol.iterator]();
+            let actionIt;
+
+            const actionIterator = {
+                [Symbol.iterator]() { 
+                    const positions = [...this.actions];
+                    return {
+                        [Symbol.iterator]() { return this },
+                        next(...args) {
+                            if (positions.length > 0) {
+                                const position = positions.shift();
+                                const result = position(...args);
+                    
+                                return { value: result, done: false}
+                            } else {
+                                return { done: true}
+                            }
+                        }
+                    }
+                },
+                actions: [askForServingSize, displayCalories]
+
+            };
+
+            function askForServingSize(food) {
+                readline.question('How many servings did you eat? (as a decimal: 1, 0.5, 1.2 etc)', servingSize => {
+                    actionIt.next(servingSize, food)
+                })
+            }
+
+             function displayCalories(servingSize = 1, food) {
+                const calories = food.calories;
+                console.log(
+                    `${food.name} with a serving size of ${servingSize} has a 
+                     ${Number.parseFloat(calories * parseInt(servingSize, 10),).toFixed()}
+                     calories
+                     `
+
+                );
+                actionIt.next()
+                readline.prompt()
+            }
+
+            readline.question(
+              "What would you like to log today?",
+              (item) => {
+
+                let position = it.next();
+                while (!position.done) {
+                  const food = position.value.name;
+                  if (food === item.trim()) {
+                    console.log(
+                      `${item} has ${position.value.calories} calories`
+                    );
+                    actionIt = actionIterator[Symbol.iterator]();
+                    actionIt.next(position.value);
+                  }
+                  position = it.next();
+                }
+
+                readline.prompt();
+              });
+
+              break;
         }
-        postion = it.next();
-    }
+    
+
+readline.prompt();
+})
 
 
-    console.log(item);
-    readline.close();
-});
+
+
